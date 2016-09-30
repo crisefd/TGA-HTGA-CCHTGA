26c400c484a02709034a488d7a14115249fb3f8b
Cristhian Fuertes <cristhian.fuertes@correounivalle.edu.co>
2016-09-30 02:52:18 +0000

    3 benchamark passed

--- helpers/test_functions.rb
+++ helpers/test_functions.rb
@@ -10,13 +10,22 @@
 module TestFunctions
   
   KNAPSACK_FUNCTION = lambda do |x, v, w, max_w|
-    sum_w = 0
+    sum_w = Array.new(w.size, 0)
     sum_v = 0
     range = (0...x.size).to_a
     while range.size > 0 
       i = range.delete_at(rand(range.size))
-      break if sum_w + x[i] * w[i] > max_w
-      sum_w +=  x[i] * w[i]
+      contraint_broke = false
+      (0...sum_w.size).each do |j|
+        if sum_w[j] + x[i] * w[j][i] > max_w[j]
+          contraint_broke = true
+          break
+        else
+          sum_w[j] += x[i]*w[j][i]
+        end
+      end
+      
+      break if contraint_broke
       sum_v += x[i] * v[i]
     end
     sum_v
