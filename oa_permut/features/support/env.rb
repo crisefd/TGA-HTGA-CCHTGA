70efdf0c15efc0fe247abe77de0f76d57bf9d29f
Cristhian Fuertes <cristhian.fuertes@correounivalle.edu.co>
2017-01-19 15:50:53 -0500

    oa_permut's tests code coverage reached 73%

--- oa_permut/features/support/env.rb
+++ oa_permut/features/support/env.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+
+SimpleCov.start do
+    add_filter '/features/'
+end
+
 require File.join(File.dirname(__FILE__), '..', '..', 'oa_permut.rb')
 require 'rspec/expectations'
 require 'matrix'
